c008de7d0fb926328c262de80af4989f75a6f43d
Sumit Howlader Dipro <96797358+SumitHD-007@users.noreply.github.com>
2023-01-09 01:58:44 +0600

    Add files via upload

---
+++ Object Recognition Robot/my_project/controllers/Mid_Controller/build/release/Mid_Controller.d
@@ -0,0 +1,8 @@
+build/release/Mid_Controller.o: Mid_Controller.cpp \
+ C:\Users\Admin\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Users\Admin\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Users/Admin/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:\Users\Admin\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Users\Admin\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Users\Admin\AppData\Local\Programs\Webots/include/controller/cpp/webots/Camera.hpp \
+ C:/Users/Admin/AppData/Local/Programs/Webots/include/controller/c/webots/camera_recognition_object.h
